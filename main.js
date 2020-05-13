71ba196d0357cb879aca1130c4290695259f5ac7
brad Traversy <bra@tranversary.com>
2020-05-13 20:28:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+    const myform=document.querySelector('#my-form');
+    const nameInput=document.querySelector('#name');
+    const emailInput=document.querySelector('#email');
+    const msg=document.querySelector('.msg');
+    const userList=document.querySelector('#users');
+    myform.addEventListener('submit',onSubmit);
+
+    function onSubmit(e) {
+        e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML='please enter all fields';
+
+        setInterval(() => msg.remove(),3000);
+    }
+    else {
+        const li =document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        nameInput.value='';
+        emailInput.value='';
+    }
+    }
